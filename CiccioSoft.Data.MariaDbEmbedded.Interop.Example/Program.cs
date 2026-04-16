3211fc6da37c5372f048c77648c46b71b50aef43
Francesco Crimi <francrim@hotmail.com>
2026-04-16 12:27:17 +0200

    fix MySqlOption

--- CiccioSoft.Data.MariaDbEmbedded.Interop.Example/Program.cs
+++ CiccioSoft.Data.MariaDbEmbedded.Interop.Example/Program.cs
@@ -5,7 +5,7 @@
 // https://opensource.org/licenses/MIT.
 
 using System;
-using CiccioSoft.Data.MariaDbEmbedded.Interop.Native;
+using CiccioSoft.Data.MariaDbEmbedded.Interop.IA;
 
 namespace CiccioSoft.Data.MariaDbEmbedded.Interop.Example;
 
@@ -17,5 +17,8 @@ class Program
         MySql mysql = MySql.Init();
         mysql.SetOption(MySqlOption.MYSQL_OPT_SSL_VERIFY_SERVER_CERT, false);
         mysql.Open("localhost", 3306, "root", "password", "testdb");
+        // Console.WriteLine($"MariaDB client version: {mysql.GetClientInfo()}");
+        // Console.WriteLine($"MariaDB server version: {mysql.GetServerInfo()}");
+        mysql.Dispose();
     }
 }
